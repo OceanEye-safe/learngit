e2a8874eb207c9073c8b21f162dc10d0a3b21072
real-leather <769918525@qq.com>
2018-08-14 13:43:54 +0800

    commit README.txt and game.py

---
+++ game.py
@@ -0,0 +1,19 @@
+import random
+
+print('-----------fish_c----------')
+secret = random.randint(1,10)
+temp = input('guess which number is he thought:')
+guess = int(temp)
+
+while guess != secret:
+    temp=input('oops ,wrong number,please try again')
+    guess=int(temp)
+    if guess == secret:
+        print('bingo!')
+    else:
+        if guess > secret:
+            print('too big')
+        else:
+            print('too small')
+
+print('GAME OVER')
